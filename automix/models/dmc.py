f4909320e0b89a1d5cf140f9dea02f76db4ac0b5
veronicags1998 <53625565+veronicags1998@users.noreply.github.com>
2024-02-14 22:02:47 -0300

    Update dmc.py

--- automix/models/dmc.py
+++ automix/models/dmc.py
@@ -257,8 +257,8 @@ class Mixer(torch.nn.Module):
         # ------------- apply eq_gain -------------
         gain_eq = p[..., 2]  # get gain parameter
         gain_eq = restore_from_0to1(gain_eq, self.min_gain_dB, self.max_gain_dB)
-        #gain_lin_eq = 10 ** (gain_eq / 20.0)  # convert gain from dB scale to linear
-        eq = pedalboard.PeakFilter(gain_db = gain_eq[0][0])
+        gain_lin_eq = 10 ** (gain_eq / 20.0)  # convert gain from dB scale to linear
+        eq = pedalboard.PeakFilter(gain_db = gain_lin_eq[0][0])
         x = eq(x, sample_rate = self.sample_rate)
         
         # ----------------- apply mix -------------
